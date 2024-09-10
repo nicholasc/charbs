65fc86c4f48c57f46e89f8db071a1f1117054a7e
Nicholas Charbonneau <nicholas.charbonneau@gmail.com>
2024-09-10 19:56:16 -0400

    assets doc

--- src/assets.rs
+++ src/assets.rs
@@ -17,12 +17,21 @@ impl AsRef<str> for Asset {
   }
 }
 
+/// A structure used to manage on disk assets.
 #[derive(Default)]
 pub struct Assets {
   storage: HashMap<&'static str, Asset>,
 }
 
 impl Assets {
+  /// Load an asset from a file path. If the asset already exists in memory, it
+  /// will not be loaded.
+  ///
+  /// TODO: Implement error handling for file reading failures.
+  ///
+  /// # Arguments
+  ///
+  /// * `path` - The file path of the asset to load.
   #[inline]
   pub fn load(&mut self, path: &'static str) {
     if !self.storage.contains_key(path) {
@@ -35,15 +44,29 @@ impl Assets {
     }
   }
 
+  /// Return a reference to the asset with the given path. If the asset does not
+  /// exist, it will be loaded.
+  ///
+  /// # Arguments
+  ///
+  /// * `path` - The path of the asset to retrieve.
+  ///
+  /// * `->` - A reference to the asset with the given path.
   pub fn get(&mut self, path: &'static str) -> &Asset {
     self.load(path);
     self.storage.get(path).unwrap()
   }
 
+  /// Unload the asset with the given path from memory.
+  ///
+  /// # Arguments
+  ///
+  /// * `path` - The path of the asset to unload.
   pub fn unload(&mut self, path: &'static str) {
     self.storage.remove(&path);
   }
 
+  /// Unload all assets from memory.
   pub fn clear(&mut self) {
     self.storage.clear();
   }
